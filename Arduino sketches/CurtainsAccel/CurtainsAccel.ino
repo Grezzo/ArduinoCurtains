958e6596d6ed6624a827d6111400b338785ee3a1
Graeme Robinson <grobinson@imap-mail.com>
2015-01-20 17:12:14 +0000

    Put in code for open/close alarm
    Made const to set speed
    Changed some vars to consts
    Removed commented out testing code
    Commented out code that prints position and target

--- Arduino sketches/CurtainsAccel/CurtainsAccel.ino
+++ Arduino sketches/CurtainsAccel/CurtainsAccel.ino
@@ -15,8 +15,14 @@ void backwardstep() {
 // Wrap the stepper in an AccelStepper object
 AccelStepper stepper(forwardstep, backwardstep);
 
-int fullsteps = 1063; // Number of steps for full open/close
+const int fullsteps = 1063; // Number of steps for full open/close
+const int stepperSpeed = 350;
 
+const unsigned long dayInMillis = 86400000; // 24 * 60 * 60 * 1000
+
+boolean alarmSet = false; // Starts off with no alarm set
+unsigned long openModulo;
+unsigned long closeModulo;
 
 void setup()
 {  
@@ -29,16 +35,25 @@ void setup()
 void loop()
 {
 
+  if (alarmSet) { // Check if alarm is set
+    unsigned long millisModulo = millis() % 86400000; // Get time past time switched on, discard any days
+    if (millisModulo == openModulo) { // Check if time since switched on is open alarm time
+      Serial.println("Open Alarm!");
+
+      moveToPosition(0); // Open
+
+    } 
+    else if (millisModulo == closeModulo) { // Check if time since switched on is close alarm time
+      Serial.println("Close Alarm!");
+
+      moveToPosition(fullsteps); //Close
+
+    }
+  }
+
   while (Serial.available() > 0) { // Check if there is data to read from the serial input
-    //    int goal = Serial.parseInt(); // Get the first number (function number)
-    //    stepper.moveTo(goal);
-    //    if (goal < stepper.currentPosition()) { // Set direction to move
-    //      stepper.setSpeed(-350);
-    //    } 
-    //    else {
-    //      stepper.setSpeed(350);
-    //    }    
     int function = Serial.parseInt(); // Get the first number (function number)
+
     if (function == 0) { // Set curtain position
       int percentage = Serial.parseInt(); //Get desired curtain position in percent
       if (percentage > 100) { //Make sure number is between 0 and 100, if not sanitise it
@@ -49,26 +64,53 @@ void loop()
       }
       int goal = fullsteps * (percentage / 100.0); // Figure out how many steps from open that percentage is
       moveToPosition(goal);  
-    } 
+    }
+
+    else if (function ==1) { // Set the alarm
+
+      unsigned long nowMillisSinceMidnight = millisSinceMidnight(Serial.parseInt(), Serial.parseInt()); // Get the time now in milliseconds since midnight
+      unsigned long openMillisSinceMidnight = millisSinceMidnight(Serial.parseInt(), Serial.parseInt()); // Get the open alarm time in milliseconds since midnight
+      unsigned long closeMillisSinceMidnight = millisSinceMidnight(Serial.parseInt(), Serial.parseInt()); // Get the close alarm time in milliseconds since midnight
+
+      Serial.print("Time now in ms is: ");
+      Serial.println(nowMillisSinceMidnight);
+      Serial.print("Open time in ms is: ");
+      Serial.println(openMillisSinceMidnight);
+      Serial.print("Close time in ms is: ");
+      Serial.println(closeMillisSinceMidnight);
+
+      unsigned long openFromNowMillis = millisFromUntil(nowMillisSinceMidnight, openMillisSinceMidnight); // Get the open alarm time in milliseconds from now
+      unsigned long closeFromNowMillis = millisFromUntil(nowMillisSinceMidnight, closeMillisSinceMidnight); // Get the close alarm time in milliseconds from now
+
+      Serial.print("Open time in from now ms is: ");
+      Serial.println(openFromNowMillis);
+      Serial.print("Close time in from now ms is: ");
+      Serial.println(closeFromNowMillis);
+
+      unsigned long now = millis(); // Get the time since power cycle in milliseconds
+
+      openModulo = moduloMillis(openFromNowMillis, now); // Get the open alarm time since power cycle, discard any days
+      closeModulo = moduloMillis(closeFromNowMillis, now); // Get the close alarm time since power cycle, discard any days
+
+      alarmSet = true; // Turn the alarm on
+    }
+
     else if (function == 2) { // Set open or closed position
       int newPosition = Serial.parseInt();
       setPosition(newPosition);
     }
   }
-
-        logPosition();
-
+  //  logPosition();
   stepper.runSpeedToPosition(); // Move stepper (if necesarry)
-
 }
 
 void moveToPosition(int goal) {
   stepper.moveTo(goal);
   if (goal < stepper.currentPosition()) { // Set direction to move
-    stepper.setSpeed(-350);
+    stepper.setSpeed(-stepperSpeed);
   } 
   else {
-    stepper.setSpeed(350);
+    stepper.setSpeed(stepperSpeed);
   }  
 }
 
@@ -77,28 +119,39 @@ void setPosition(int newPosition) {
   stepper.moveTo(newPosition); // Set goal position
 }
 
-boolean printed = false;
-void logPosition() {
-  if (millis() % 1000 == 0) {
-    if (printed == false) {
-      Serial.print("Target Position: ");
-      Serial.println(stepper.targetPosition());
-      Serial.print("Current Position: ");
-      Serial.println(stepper.currentPosition());
-      Serial.print("Distance To Go: ");
-      Serial.println(stepper.distanceToGo());
-
-      printed = true;
-    }
-  } 
-  else {
-    printed = false;
-  } 
+unsigned long millisSinceMidnight(int hours, int mins) { //Get time in milliseconds since midnight
+  return ((unsigned long)hours * 60 + mins) * 60 * 1000;
 }
 
+unsigned long millisFromUntil(unsigned long from, unsigned long until) { // Get milliseconds from one time until another (second time can be earlier, so would be next day)
+  long difference = until - from;
+  if (difference < 0) { // Time is next day
+    return difference + dayInMillis;
+  } 
+  else { // Time is later today
+    return difference;
+  }
+}
 
+unsigned long moduloMillis(unsigned long millisFromNow, unsigned long now) { // Get milliseconds for a time since power cycle, discard any days
+  return (millisFromNow + now) % dayInMillis;
+}
 
-
-
-
-
+//boolean printed = false;
+//void logPosition() {
+//  if (millis() % 1000 == 0) {
+//    if (printed == false) {
+//      Serial.print("Target Position: ");
+//      Serial.println(stepper.targetPosition());
+//      Serial.print("Current Position: ");
+//      Serial.println(stepper.currentPosition());
+//      Serial.print("Distance To Go: ");
+//      Serial.println(stepper.distanceToGo());
+//
+//      printed = true;
+//    }
+//  } 
+//  else {
+//    printed = false;
+//  } 
+//}
